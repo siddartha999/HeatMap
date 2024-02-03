70934d08ebf7e378a714739c417493c5c2b4128d
Siddartha Tummala <sidddddd@amazon.com>
2024-02-03 12:19:50 -0800

    Initil project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HeatMap from './HeatMap'
+
+function App() {
+
+  return (
+    <div>
+      <HeatMap />
+    </div>
+  )
+}
+
+export default App
---
+++ src/HeatMap.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const HeatMap = () => {
+    return (
+        <div>
+            HeatMap
+        </div>
+    );
+};
+
+export default HeatMap;
